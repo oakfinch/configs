7af3e5925ffcae8732008b6822bb8d80079b5a38
Nate Volker <natevolker@gmail.com>
2021-10-25 17:58:01 -0500

    fix

--- packages/eslint-config/src/javascript.js
+++ packages/eslint-config/src/javascript.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["./base.js", "plugin:prettier/recommended"]
+  extends: ["../src/base.js", "plugin:prettier/recommended"]
 };
--- packages/eslint-config/src/typescript.js
+++ packages/eslint-config/src/typescript.js
@@ -1,24 +1,32 @@
 module.exports = {
   extends: [
-    "./base.js",
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
+    "../src/javascript.js",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".ts", ".tsx"],
+  overrides: [
+    {
+      "files": ["src/**/*.ts", "src/**/*.tsx"],
+      extends: [
+        "../src/base.js",
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+        project: "./tsconfig.json",
       },
-    },
-    "import/extensions": [".ts", ".tsx"],
-  },
+      plugins: ["@typescript-eslint"],
+      settings: {
+        "import/resolver": {
+          node: {
+            extensions: [".ts", ".tsx"],
+          },
+        },
+        "import/extensions": [".ts", ".tsx"],
+      },
+    }
+  ],
 };
